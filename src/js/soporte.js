8e18967c9d2b992c1f493b4aabcce5ee87e94a46
cristian <cristian35comp@hotmail.com>
2021-11-17 19:18:14 -0300

    el -am no funciono, tuve que usar add, no agrego js/soporte.js

---
+++ src/js/soporte.js
@@ -0,0 +1,5 @@
+const dat=document.getElementById('datos');
+let elemento=document.createElement('h3');
+elemento.textContent="hola comunicate al siguiente numero: .....";
+dat.appendChild(elemento);
+dat.parentElement.className="article";
