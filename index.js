8c181c0ead0c4ab7f1e0d222c1af3e45cad12819
cristian <cristian35comp@hotmail.com>
2021-11-19 04:21:38 -0300

    convertido para funcionar y fix the package to begin 2

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const path=require('path')
 const express=require("express")
 const app=express()
+
 // app.set('views','./'); //en los set el parametro de la izquierda no es arbitrario se respeta el views
+app.set('port', process.env.PORT || 80);
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'src/views/index.html'));
 })
-app.use(express.static('./src'));
+app.use(express.static(path.join(__dirname,'src')));
 
 app.use((req,res,next)=>{
   res.status(404).send('no existe esta ruta');//parte para el navegador , otra parte para usuario
 });
 
-app.listen('80')
+app.listen(app.get('port'))
